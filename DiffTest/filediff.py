3dc40425e62608f06f6a04437bcfcccdfffcd52c
esspee39 <shadowphoenix39@gmail.com>
2021-12-19 14:48:00 -0500

    Modify to be function

--- DiffTest/filediff.py
+++ DiffTest/filediff.py
@@ -1,15 +1,16 @@
 import difflib as dl
 
-with open('example.cpp') as f:
-    original_file = f.read()
+def generateHTMLDiff(srcname, mutantname):
+    with open("../src/" + srcname) as f:
+        original_file = f.read()
 
-with open('example_ArithmeticPlusToMinus.cpp') as f:
-    mutated_file = f.read()
+    with open("../mutants/" + mutantname + "/src/" + srcname.replace(".cpp", "_" + mutantname + ".cpp")) as f:
+        mutated_file = f.read()
 
-hd = dl.HtmlDiff()
+    hd = dl.HtmlDiff()
 
-diffs = hd.make_file(original_file.split("\n"), mutated_file.split("\n"), fromdesc='Source', todesc='Mutant', context=False, numlines=0)
+    diffs = hd.make_file(original_file.split("\n"), mutated_file.split("\n"), fromdesc='Source', todesc=mutantname, context=False, numlines=0)
 
-with open('diff.html',"w+") as diff_file:
-    diff_file.write(diffs)
+    with open("diff" + "_" + mutantname + ".html","w+") as diff_file:
+        diff_file.write(diffs)
 
